4d155b767592b7d157100a74c53e0326f3ae9b95
Ivan Garavito <ivangaravito@gmail.com>
2012-10-26 23:55:28 -0500

    Initial code commit

---
+++ etc/config.defaults.js
@@ -0,0 +1,23 @@
+module.exports = {
+    logLevel: 0xF7            //Log level. Value 0 means disable logging. Flags:
+                                //	Info			0x01
+                                //	Error			0x02
+                                //	Debug			0x04
+                                //	Connections		0x10
+                                //	DataEvents		0x20
+                                //	IncomingData	0x40
+                                //	OutcomingData	0x80
+    , listenRetryTimeout: 500 //Wait retryTimeout milliseconds before retry listening
+    , listenRetryTimes: 1     //Retry retryTimes times when trying to listen
+    , connRetryTimeout: 1000  //Wait retryTimeout milliseconds before retry connection
+    , connRetryTimes: 2       //Retry retryTimes times
+    , localHost: '127.0.0.1'  //IP for listening to connections
+    , pool: {/*               //Here you define 
+        <listen_at_port>: {
+            dstHost: '<to_host>'
+          , dstPort: <to_port>
+          , srcHost: '<from_host>'
+          , srcPort: <from_port>
+        }
+    */}
+}
---
+++ noderelay.js
@@ -0,0 +1,166 @@
+/*
+	LIBRARIES
+*/
+var _ = require('underscore')._
+  , net = require('net')
+
+/*
+	CONFIG
+*/
+var cfg = require('./etc/config') || {}                     //User defined config
+  , cfgDefaults = require('./etc/config.defaults') || {}    //Default config
+  , connList = []           //List of socket connections
+  , hostTrack = {}          //Track of hosts connected to which server from the pool
+  , serverPool = {}         //List of servers accepting connections
+
+//Prepares configuration
+cfg = _.defaults(cfg, cfgDefaults)
+
+/*
+	INITIALIZATION
+*/
+console.log('Initializing...')
+
+//Catches the process exit
+process.on('exit', _exit)
+
+//Sends uncaught exceptions to console
+process.on('uncaughtException', function (err) {
+	console.error(err)
+})
+
+console.log('Creating server pool...')
+
+//Creates server pool
+_.each(cfg.pool, function (params, port) {
+    console.log('Creating new server for ' + cfg.localHost + ':' + port + '...')
+
+    //Creates a new server
+    var server = serverPool[port] = net.createServer()
+
+    //Sets maximum retry times
+    server.listenRetryTimes = cfg.listenRetryTimes
+
+    //Sets server name to the port
+    server.name = port
+
+    //Sets server listening event handler
+    server.on('listening', function () {
+        console.log('NodeRelay iniciado y esperando conexiones en ' + this.address().address + ':' + this.address().port)
+    })
+
+    //Sets server connection event handler
+    server.on('connection', function (socket) {
+        var dstOptions                  //Destiny connection options
+          , dstSocket                   //Socket for destiny connection
+          , name = socket.remoteAddress + ':' + socket.remotePort   //Connection name is host:port
+          , stamp = new Date()          //Local connection time stamp
+
+        console.log('New connection from ' + name)
+
+        //Tracks remote host to which port is connected
+        hostTrack[socket.remoteAddress] = server.name
+
+        //Prepares socket options and makes destiny connection
+        dstOptions = makeConnectionOptions(params.dstPort, params.dstHost, params.srcHost)
+        dstSocket = net.connect(dstOptions)
+
+        //Connection event handler
+        dstSocket.on('connect', function () {
+            console.log('Remote connection successfully. Creating pipe ' +
+                name + ' <=> ' + params.dstHost + ':' + params.dstPort
+            )
+            var self = this
+            //Pipes local/destiny sockets
+            self.pipe(socket).pipe(self)
+        })
+
+        //When server accepts a new connection, the new socket receives data
+        //before detiny connection is established
+        socket.once('data', function (buffer) {
+            dstSocket.write(buffer)
+        })
+
+        //Connection closed event handler
+        socket.on('close', function (had_error) {
+            console.log('Pipe ' +
+                name + ' <=> ' + params.dstHost + ':' + params.dstPort + ' finished'
+            )
+            delete connList[name]
+        })
+
+        //Records connections
+        connList[name] = {
+            sockets: [socket, dstSocket]
+          , stamp: stamp
+        }
+    })
+    //Server closed
+    server.on('close', function () {
+    })
+    //Server error handling
+    server.on('error', function (e) {
+        //Checks if cannot bind to port
+        if (e.code == 'EADDRINUSE') {
+            var retries = this.listenRetryTimes--   //Retry times left
+            //Can we retry?
+            if (retries > 0) {
+                console.error('Address in use, retrying in ' + cfg.listenRetryTimeout + 'ms')
+                //Schedule next retry
+                setTimeout(server.startListen, cfg.listenRetryTimeout)
+            } else {
+                console.error('Cannot bind NodeRelay at ' + cfg.localHost + ':' + this.name + '. Exiting!')
+                process.exit(1)
+            }
+        } else {
+            console.error(e)
+        }
+    })
+    server.startListen = function () {
+        if(cfg.localHost === '0.0.0.0') {
+            console.log('Binding server to 0.0.0.0:' + this.name + '...')
+            server.listen(this.name)
+        } else {
+            console.log('Binding server to ' + cfg.localHost + ':' + this.name + '...')
+            server.listen(this.name, cfg.localHost)
+        }
+    }
+})
+
+//Commands each serverat pool to start listening
+_.invoke(serverPool, 'startListen')
+
+console.log('NodeRelay started!')
+
+/*
+    APPLICATION FUNCTIONS
+*/
+function _exit() {
+    console.log('Terminando NodeRelay...')
+
+    //Closing each server at pool
+    console.log('Closing servers...')
+    _.invoke(serverPool, 'close')
+
+    console.log('Closing connections...')
+
+    //Closing connections
+    _.each(connList, function (item) {
+        console.log('Killing worker ' + item.name + '...')
+        item.sockets.end()
+    })
+
+    console.log('NodeRelay terminated!')
+}
+
+function makeConnectionOptions(port, host, localAddress) {
+    var opt = {
+        port: port  //Remote port to connect to
+      , host: host  //Remote host to connect to
+    }
+    //Checks if defined a local address to get out
+    if (!_.isEmpty(localAddress) && localAddress !== '0.0.0.0') {
+        opt.localAddress = localAddress
+    }
+    return opt
+}
